427aaa2722215a843dc79e54b486211fe02c4b20
SwiftProGrammer178611 <165575838+SwiftProGrammer178611@users.noreply.github.com>
2026-07-21 00:04:28 -0400

    add production build script for vercel deployment

--- warioSer/webpack.config.js
+++ warioSer/webpack.config.js
@@ -1,49 +1,57 @@
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-  mode: 'development',
-  entry: './src/js/canvas.js',
-  output: {
-    path: __dirname + '/dist/',
-    filename: './js/canvas.bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production'
+
+  return {
+    mode: argv.mode || 'development',
+    entry: './src/js/canvas.js',
+    output: {
+      path: __dirname + '/dist/',
+      filename: './js/canvas.bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
           }
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader'
+            }
+          ]
         }
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader'
-          }
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new BrowserSyncPlugin({
-      host: 'localhost',
-      port: 3000,
-      server: { baseDir: ['dist'] },
-      files: ['./dist/*'],
-      notify: false
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      favicon: 'favicon.ico',
-      template: 'src/index.html'
-    })
-  ],
-  watch: true,
-  devtool: 'source-map'
+      ]
+    },
+    plugins: [
+      ...(isProduction
+        ? []
+        : [
+            new BrowserSyncPlugin({
+              host: 'localhost',
+              port: 3000,
+              server: { baseDir: ['dist'] },
+              files: ['./dist/*'],
+              notify: false
+            })
+          ]),
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        favicon: 'favicon.ico',
+        template: 'src/index.html'
+      })
+    ],
+    watch: !isProduction,
+    devtool: isProduction ? false : 'source-map'
+  }
 }
