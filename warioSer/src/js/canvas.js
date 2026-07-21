6f2af72b375d7a63a3eba6054316c8045cf47791
SwiftProGrammer178611 <165575838+SwiftProGrammer178611@users.noreply.github.com>
2026-07-20 23:04:03 -0400

    Added Wario source files and assets

---
+++ warioSer/src/js/canvas.js
@@ -0,0 +1,173 @@
+import platform from '/platform.png'
+
+
+console.log(platform)
+
+
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+console.log(canvas)
+
+canvas.width = 1084
+canvas.height = 576
+const gravity = 0.5
+
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100
+    }
+    this.width = 30
+    this.height = 30
+
+    this.velocity = {
+      x: 0,
+      y: 1
+    }
+  }
+
+  draw() {
+    c.fillStyle = 'black'
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+  }
+
+  update() {
+    this.draw()
+    this.position.y += this.velocity.y
+    this.position.x += this.velocity.x
+
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+      this.velocity.y += gravity
+    } else {
+      this.velocity.y = 0
+    }
+  }
+}
+class Platform {
+  constructor({ x, y, image}) {
+    this.position = {
+      x,
+      y
+    }
+    this.image = image
+    this.width = this.image.width
+    this.height = this.image.height
+    
+  }
+
+  draw() {
+    c.drawImage(this.image, this.position.x, this.position.y)
+  }
+}
+
+const image = new Image()
+image.src = platform
+
+const player = new Player()
+const platforms = [new Platform({
+  x: -1, y: 470 , image: image
+}), new Platform({ x: image.width-3, y: 470, image:image })]
+const keys = {
+  right: {
+    pressed: false
+  },
+  left: {
+    pressed: false
+  }
+}
+
+//how far player moved
+let scrollOffset = 0
+
+player.draw()
+
+function animate() {
+  requestAnimationFrame(animate)
+  c.fillStyle = 'white'
+  c.fillRect(0, 0, canvas.width, canvas.height)
+  
+  platforms.forEach(platform => {
+    platform.draw()
+  })
+
+  player.update()
+
+  
+
+  if (keys.right.pressed && player.position.x < 400) {
+    player.velocity.x = 5
+  } else if (keys.left.pressed && player.position.x > 100) {
+    player.velocity.x = -5
+  } else {
+    player.velocity.x = 0
+
+    if (keys.right.pressed) {
+      scrollOffset += 5
+      platforms.forEach((platform) => {
+        platform.position.x -= 5
+      })
+    } else if (keys.left.pressed) {
+      platforms.forEach((platform) => {
+        platform.draw()
+        platform.position.x += 5
+      })
+    }
+  }
+
+  console.log(scrollOffset)
+
+  platforms.forEach((platform) => {
+    if (player.position.y + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width) {
+      player.velocity.y = 0
+    }
+  })
+
+  if (scrollOffset > 2000) {
+    console.log('you win')
+  }
+}
+animate()
+
+addEventListener('keydown', ({ keyCode }) => {
+  console.log(keyCode)
+  switch (keyCode) {
+    case 65:
+      console.log('left')
+      keys.left.pressed = true
+      break
+    case 83:
+      console.log('down')
+      break
+    case 68:
+      console.log('right')
+      keys.right.pressed = true
+      break
+    case 87:
+      console.log('up')
+      player.velocity.y -= 2
+      break
+  }
+})
+
+addEventListener('keyup', ({ keyCode }) => {
+  console.log(keyCode)
+  switch (keyCode) {
+    case 65:
+      console.log('left')
+      keys.left.pressed = false
+      break
+    case 83:
+      console.log('down')
+      break
+    case 68:
+      console.log('right')
+      keys.right.pressed = false
+      break
+    case 87:
+      console.log('up')
+      player.velocity.y -= 10
+      break
+  }
+})
